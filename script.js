de4c32057550ae09ab1203dd93511f341e45d54c
fabiya noor <fabiyanoor7@gmail.com>
2024-09-15 12:46:44 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const menuBtn = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.navbar');
+
+menuBtn.onclick = () => {
+    navbar.classList.toggle('active');
+    menuBtn.classList.toggle('fa-times'); // Changes icon
+};
+var swiper = new Swiper(".course-slider", {
+    
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
+
+  var swiper = new Swiper(".teachers-slider", {
+    
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    
+    spaceBetween: 20,
+    grabCursor:true,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+  });
